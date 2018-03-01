a4a1d617650d2dfdc9bf191dd78e96e054133fb3
rtullybarr <rtullybarr@gmail.com>
2018-03-01 14:54:37 -0800

    Adding some utility functions to clean up snake avoidange.

--- app/collisions.py
+++ app/collisions.py
@@ -5,6 +5,24 @@ LEFT = 2
 RIGHT = 3
 
 
+# checks if two points are equal
+def points_equal(p1, p2):
+    return p1["x"] == p2["x"] and p1["y"] == p2["y"]
+
+
+# returns a new point that has been shifted one step in the given direction.
+def move_point(point, direction):
+    new_point = dict(point)
+    if direction == UP:
+        new_point["y"] -= 1
+    if direction == DOWN:
+        new_point["y"] += 1
+    if direction == LEFT:
+        new_point["x"] -= 1
+    if direction == RIGHT:
+        new_point["x"] += 1
+    return new_point
+
 # set of weighting fuctions designed to help us avoid collisions.
 def avoid_walls(data):
     criteria = {"goal": "avoid_walls", "weight": 0.5}
@@ -13,25 +31,29 @@ def avoid_walls(data):
     # where we are
     us = data["you"]
     # first point in list is our head.
-    head_location = us["body"]["data"][0]
+    our_head = us["body"]["data"][0]
 
     # possible directions we can move
     directions = [1.0, 1.0, 1.0, 1.0]
 
-    if head_location["x"] + 1 >= data["width"]:
+    if our_head["x"] + 1 >= data["width"]:
         directions[RIGHT] = 0.0
 
-    if head_location["x"] - 1 < 0:
+    if our_head["x"] - 1 < 0:
         directions[LEFT] = 0.0
 
-    if head_location["y"] + 1 >= data["height"]:
+    if our_head["y"] + 1 >= data["height"]:
         directions[DOWN] = 0.0
 
-    if head_location["y"] - 1 < 0:
+    if our_head["y"] - 1 < 0:
         directions[UP] = 0.0
 
     # normalize weighting matrix
-    criteria["direction_values"] = [x / sum(directions) for x in directions]
+    if sum(directions) == 0:
+        criteria["direction_values"] = [0.0, 0.0, 0.0, 0.0]
+    else:
+        criteria["direction_values"] = [x / sum(directions) for x in directions]
+
     return criteria
 
 
@@ -43,14 +65,12 @@ def avoid_other_snakes(data):
     us = data["you"]
     # first point in list is our head.
     us_points = us["body"]["data"]
-    head_location = us_points[0]
+    our_head = us_points[0]
 
     # spots we could move:
 
-    moves = [(head_location["x"], head_location["y"] - 1),  # up
-             (head_location["x"], head_location["y"] + 1),  # down
-             (head_location["x"] - 1, head_location["y"]),  # left
-             (head_location["x"] + 1, head_location["y"])]  # right
+    moves = [move_point(our_head, UP), move_point(our_head, DOWN),
+             move_point(our_head, LEFT), move_point(our_head, RIGHT)]
 
     # other snakes
     other_snakes = data["snakes"]["data"]
@@ -65,33 +85,25 @@ def avoid_other_snakes(data):
         for index, point in enumerate(snake_points):
 
             # special handling for enemy snake heads
-            if index == 0:
+            if index == 0 and not points_equal(our_head, point):
 
-                weight = 0
+                weight = 0.0
                 if len(snake_points) < len(us_points):
-                    weight = 2
+                    weight = 2.0
 
                 for i in range(4):
-                    # they move up
-                    if moves[i][0] == point["x"] and moves[i][1] == point["y"] - 1:
-                        directions[i] = weight
-
-                    # they move down
-                    if moves[i][0] == point["x"] and moves[i][1] == point["y"] + 1:
-                        directions[i] = weight
-
-                    # they move left
-                    if moves[i][0] == point["x"] - 1 and moves[i][1] == point["y"]:
-                        directions[i] = weight
-
-                    # they move right
-                    if moves[i][0] == point["x"] + 1 and moves[i][1] == point["y"]:
-                        directions[i] = weight
+                    for j in range(4):
+                        if points_equal(moves[i], move_point(point, j)):
+                            directions[i] = weight
 
             for i in range(4):
-                if moves[i][0] == point["x"] and moves[i][1] == point["y"]:
-                    directions[i] = 0
+                if points_equal(moves[i], point):
+                    directions[i] = 0.0
 
     # normalize and return
-    criteria["direction_values"] = [x / sum(directions) for x in directions]
+    if sum(directions) == 0:
+        criteria["direction_values"] = [0.0, 0.0, 0.0, 0.0]
+    else:
+        criteria["direction_values"] = [x / sum(directions) for x in directions]
+
     return criteria
