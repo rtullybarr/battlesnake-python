3a4e144edba65efe6bf1460464651ff0de35f256
rtullybarr <rtullybarr@gmail.com>
2018-03-01 16:14:54 -0800

    Including move to most space as a priority.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import bottle
 import os
-import collisions
 import random
+
+import collisions
+import freedom
 
 
 @bottle.route('/')
@@ -73,6 +75,7 @@ def get_direction_weights(data):
     # The direction weights and decision weights added at the same time,
     weights.append(collisions.avoid_walls(data))
     weights.append(collisions.avoid_other_snakes(data))
+    weights.append(freedom.move_to_most_space(data))
 
     print(weights)
 
